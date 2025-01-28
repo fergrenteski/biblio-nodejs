c196f1b9e2ae3a527223e54c4651cb718f31de54
Luiz Fernando <luizgrenfer@gmail.com>
2025-01-27 22:53:10 -0300

    :sparkles: feat:  - Adicionado tratamento de parágrafo;

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import fs from 'fs';
+import fs from "fs";
 
-const arquivo = './arquivos/texto-web.txt';
+const arquivo = "./arquivos/texto-web.txt";
 
-fs.readFile(arquivo, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
+fs.readFile(arquivo, "utf8", (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    quebraEmParagrafos(data);
 });
+
+function quebraEmParagrafos(texto) {
+    const paragrafos = texto.toLowerCase().split("\n");
+    const contagem = paragrafos.map((paragrafo) => {
+        return verificaPalavraDuplicada(paragrafo);
+    });
+    console.log(contagem);
+}
+
+
+function verificaPalavraDuplicada(texto) {
+    const palavras = texto.split(" ");
+    const resultado = {};
+    palavras.forEach((palavra) => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1;
+    });
+    return resultado;
+}
